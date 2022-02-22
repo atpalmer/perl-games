b26ab9b80cc0253cbeab78499b7daf85256af60f
Andrew Palmer <atp@sdf.org>
2022-02-21 23:03:30 -0600

    Update package syntax

--- cards.pl
+++ cards.pl
@@ -3,182 +3,179 @@
 use strict;
 use warnings;
 
-my $deck = Deck->new();
+package main {
+    my $deck = Deck->new();
 
-my $board = Board->deal($deck);
+    my $board = Board->deal($deck);
 
-print "$board\n";
+    print "$board\n";
 
-printf "%20s: %s\n", "Pair", Rank::repr($board->of_a_kind_rank(2));
-printf "%20s: %s\n", "Two Pair", Rank::repr($board->two_pair_ranks());
-printf "%20s: %s\n", "3 of a Kind", Rank::repr($board->of_a_kind_rank(3));
-printf "%20s: %s\n", "Straight", Rank::repr($board->straight_ranks());
-printf "%20s: %s\n", "Flush", Rank::repr($board->flush_ranks());
-printf "%20s: %s\n", "Full House",  Rank::repr($board->full_house_ranks());
-printf "%20s: %s\n", "4 of a Kind", Rank::repr($board->of_a_kind_rank(4));
-printf "%20s: %s\n", "Straight Flush", Rank::repr($board->straight_flush_ranks());
-
-
-package Rank;
-
-use constant RANKS => qw<2 3 4 5 6 7 8 9 T J Q K A>;
-
-sub repr {
-    return join ', ', map { (RANKS)[$_] } @_;
+    printf "%20s: %s\n", "Pair", Rank::repr($board->of_a_kind_rank(2));
+    printf "%20s: %s\n", "Two Pair", Rank::repr($board->two_pair_ranks());
+    printf "%20s: %s\n", "3 of a Kind", Rank::repr($board->of_a_kind_rank(3));
+    printf "%20s: %s\n", "Straight", Rank::repr($board->straight_ranks());
+    printf "%20s: %s\n", "Flush", Rank::repr($board->flush_ranks());
+    printf "%20s: %s\n", "Full House",  Rank::repr($board->full_house_ranks());
+    printf "%20s: %s\n", "4 of a Kind", Rank::repr($board->of_a_kind_rank(4));
+    printf "%20s: %s\n", "Straight Flush", Rank::repr($board->straight_flush_ranks());
 }
 
+package Rank {
+    use constant RANKS => qw<2 3 4 5 6 7 8 9 T J Q K A>;
 
-package Suit;
-
-use constant SUITS => qw<s h d c>;
-
-sub repr {
-    my $x = shift;
-    return (SUITS)[$x];
+    sub repr {
+        return join ', ', map { (RANKS)[$_] } @_;
+    }
 }
 
+package Suit {
+    use constant SUITS => qw<s h d c>;
 
-package Card;
-
-use overload (q/""/ => \&str);
-
-sub new {
-    my $class = shift;
-    my $self = shift;
-    return bless \$self, $class;
+    sub repr {
+        my $x = shift;
+        return (SUITS)[$x];
+    }
 }
 
-sub rankx {
-    my $self = shift;
-    return $$self % 13;
-}
+package Card {
+    use overload (q/""/ => \&str);
 
-sub suitx {
-    use integer;
-    my $self = shift;
-    return $$self / 13;
-}
-
-sub str {
-    my $self = shift;
-    return Rank::repr($self->rankx) . Suit::repr($self->suitx);
-}
-
-
-package Deck;
-
-use List::Util qw<shuffle>;
-
-sub new {
-    my $class = shift;
-    my @cards = shuffle map { Card->new($_) } (0..51);
-    return bless \@cards, $class;
-};
-
-sub deal {
-    my $self = shift;
-    my $count = shift;
-    return splice(@$self, 0, $count);
-}
-
-
-package Board;
-
-use List::Util qw<any all>;
-
-use overload (q/""/ => \&str);
-
-sub deal {
-    my $class = shift;
-    my $deck = shift;
-
-    my @board = $deck->deal(5);
-
-    # rankx => rankx count
-    my @ranks = (0) x 13;
-    $ranks[$_->rankx]++ for (@board);
-
-    # "of a kind" => rankx array ref
-    my @kinds = ([], [], [], [], []);
-    push(@{$kinds[$ranks[$_]]}, $_) for (0..$#ranks);
-
-    my $flush_suit = $board[0]->suitx;
-    if (not all { $_->suitx == $flush_suit } @board[1..$#board]) {
-        $flush_suit = undef;
+    sub new {
+        my $class = shift;
+        my $self = shift;
+        return bless \$self, $class;
     }
 
-    return bless {
-        cards => \@board,
-        ranks => \@ranks,
-        kinds => \@kinds,
-        flush_suit => $flush_suit,
-    }, $class;
-}
-
-sub str {
-    my $self = shift;
-    return join ', ', @{$self->{cards}};
-}
-
-sub of_a_kind_rank {
-    my $self = shift;
-    my $kind = shift;
-    my $ranks = $self->{kinds}[$kind];
-    if (int(@{$ranks}) != 1) {
-        return ();
+    sub rankx {
+        my $self = shift;
+        return $$self % 13;
     }
-    return @{$ranks};
+
+    sub suitx {
+        use integer;
+        my $self = shift;
+        return $$self / 13;
+    }
+
+    sub str {
+        my $self = shift;
+        return Rank::repr($self->rankx) . Suit::repr($self->suitx);
+    }
 }
 
-sub two_pair_ranks {
-    my $self = shift;
-    my $ranks = $self->{kinds}[2];
-    if (int(@{$ranks}) != 2) {
-        return ();
+package Deck {
+    use List::Util qw<shuffle>;
+
+    sub new {
+        my $class = shift;
+        my @cards = shuffle map { Card->new($_) } (0..51);
+        return bless \@cards, $class;
     }
-    return @{$ranks};
+
+    sub deal {
+        my $self = shift;
+        my $count = shift;
+        return splice(@$self, 0, $count);
+    }
 }
 
-sub full_house_ranks {
-    my $self = shift;
-    my $three = $self->of_a_kind_rank(3);
-    my $two = $self->of_a_kind_rank(2);
-    if (!$three or !$two) {
-        return ();
-    }
-    return ($three, $two);
-}
+package Board {
+    use List::Util qw<any all>;
 
-sub straight_ranks {
-    my $self = shift;
-    my $ranks = $self->{ranks};
-    my @results = ();
-    if ($ranks->[$#{$ranks}]) {
-        push(@results, $#{$ranks});
-    }
-    for (0..$#{$ranks}) {
-        if ($ranks->[$_] > 0) {
-            push(@results, $_);
-        } else {
-            @results = ();
+    use overload (q/""/ => \&str);
+
+    sub deal {
+        my $class = shift;
+        my $deck = shift;
+
+        my @board = $deck->deal(5);
+
+        # rankx => rankx count
+        my @ranks = (0) x 13;
+        $ranks[$_->rankx]++ for (@board);
+
+        # "of a kind" => rankx array ref
+        my @kinds = ([], [], [], [], []);
+        push(@{$kinds[$ranks[$_]]}, $_) for (0..$#ranks);
+
+        my $flush_suit = $board[0]->suitx;
+        if (not all { $_->suitx == $flush_suit } @board[1..$#board]) {
+            $flush_suit = undef;
         }
-        return @results if int(@results) == 5;
-    }
-    return ();
-}
 
-sub flush_ranks {
-    my $self = shift;
-    if (!defined($self->{flush_suit})) {
+        return bless {
+            cards => \@board,
+            ranks => \@ranks,
+            kinds => \@kinds,
+            flush_suit => $flush_suit,
+        }, $class;
+    }
+
+    sub str {
+        my $self = shift;
+        return join ', ', @{$self->{cards}};
+    }
+
+    sub of_a_kind_rank {
+        my $self = shift;
+        my $kind = shift;
+        my $ranks = $self->{kinds}[$kind];
+        if (int(@{$ranks}) != 1) {
+            return ();
+        }
+        return @{$ranks};
+    }
+
+    sub two_pair_ranks {
+        my $self = shift;
+        my $ranks = $self->{kinds}[2];
+        if (int(@{$ranks}) != 2) {
+            return ();
+        }
+        return @{$ranks};
+    }
+
+    sub full_house_ranks {
+        my $self = shift;
+        my $three = $self->of_a_kind_rank(3);
+        my $two = $self->of_a_kind_rank(2);
+        if (!$three or !$two) {
+            return ();
+        }
+        return ($three, $two);
+    }
+
+    sub straight_ranks {
+        my $self = shift;
+        my $ranks = $self->{ranks};
+        my @results = ();
+        if ($ranks->[$#{$ranks}]) {
+            push(@results, $#{$ranks});
+        }
+        for (0..$#{$ranks}) {
+            if ($ranks->[$_] > 0) {
+                push(@results, $_);
+            } else {
+                @results = ();
+            }
+            return @results if int(@results) == 5;
+        }
         return ();
     }
-    return map { $_->rankx } @{$self->{cards}};
-}
 
-sub straight_flush_ranks {
-    my $self = shift;
-    if (!defined($self->{flush_suit})) {
-        return ();
+    sub flush_ranks {
+        my $self = shift;
+        if (!defined($self->{flush_suit})) {
+            return ();
+        }
+        return map { $_->rankx } @{$self->{cards}};
     }
-    return $self->straight_ranks();
+
+    sub straight_flush_ranks {
+        my $self = shift;
+        if (!defined($self->{flush_suit})) {
+            return ();
+        }
+        return $self->straight_ranks();
+    }
 }
